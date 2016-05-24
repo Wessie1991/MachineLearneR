19a04ba6678d63810ab18d08e6042a00a61245a7
Wessie1991 <Desmond1991@gmail.com>
2016-05-24 10:53:13 +0200

    hallo

--- R/applyFactorLoadingModel.R
+++ R/applyFactorLoadingModel.R
@@ -7,7 +7,7 @@
 #' @export
 applyFactorLoadingModel <- function(mydata, solution, factorNames, faMethodScores){
   mydataMatrix <- as.matrix(mydata)
-  #ik Ben desmonda 
+  #ik Ben desmonda  
   covx <- rrcov::Cov(mydataMatrix)
 
   covList <- list(cov = rrcov::getCov(covx), center = rrcov::getCenter(covx))
